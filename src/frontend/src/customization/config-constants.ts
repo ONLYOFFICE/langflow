a244a778297698481f4054d71040aa41ff37a74c
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2025-02-22 02:03:49 +0400

    customization: change BASENAME

--- src/frontend/src/customization/config-constants.ts
+++ src/frontend/src/customization/config-constants.ts
@@ -1,9 +1,9 @@
-export const BASENAME = "";
+export const BASENAME = "/onlyflow/";
 export const PORT = 3000;
 export const PROXY_TARGET = "http://127.0.0.1:7860";
-export const API_ROUTES = ["^/api/v1/", "/api/v2/", "/health"];
-export const BASE_URL_API = "/api/v1/";
-export const HEALTH_CHECK_URL = "/health_check";
+export const API_ROUTES = [`^${BASENAME}api/v1/`, `${BASENAME}api/v2/`, `${BASENAME}health`];
+export const BASE_URL_API = `${BASENAME}api/v1/`;
+export const HEALTH_CHECK_URL = `${BASENAME}health_check`;
 export const DOCS_LINK = "https://docs.langflow.org";
 
 export default {
