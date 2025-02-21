b83eda551b0a86025d9b3262e0560e90d76f131c
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2025-02-22 02:22:59 +0400

    models: Add togetherai

---
+++ src/backend/base/langflow/base/models/togetherai_constants.py
@@ -0,0 +1,9 @@
+TOGETHERAI_MODEL_NAMES = [
+    "meta-llama/Llama-3.3-70B-Instruct-Turbo-Free",
+    "deepseek-ai/DeepSeek-R1-Distill-Llama-70B-free",
+]
+TOGETHERAI_EMBEDDING_MODEL_NAMES = [
+]
+
+# Backwards compatibility
+MODEL_NAMES = TOGETHERAI_MODEL_NAMES
