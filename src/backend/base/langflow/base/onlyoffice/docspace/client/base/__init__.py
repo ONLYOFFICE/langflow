3f230027b690759796a217fa24c3b0e7193a7202
vanyauhalin <ivan.uhalin@onlyoffice.com>
2025-03-27 16:47:36 +0400

    add filters structure for docspace client

--- src/backend/base/langflow/base/onlyoffice/docspace/client/base/__init__.py
+++ src/backend/base/langflow/base/onlyoffice/docspace/client/base/__init__.py
@@ -4,17 +4,21 @@ from .response import ErrorPayload, ErrorResponse, Response, ResponseError, Resp
 from .service import Service
 from .transformer import Transformer, TransformerHandler
 from .formdata import encode_multipart_formdata
+from .filters import FilterOp, Filters, SortOrder
 
 
 __all__ = [
     "Client",
     "ErrorPayload",
     "ErrorResponse",
+    "FilterOp",
+    "Filters",
     "Opener",
     "Response",
     "ResponseError",
     "ResponseLink",
     "Service",
+    "SortOrder",
     "SuccessPayload",
     "SuccessResponse",
     "Transformer",
