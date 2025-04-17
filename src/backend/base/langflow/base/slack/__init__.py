0e595cc1c01b86f61dbd769b466891c5df783886
sshakndr <aleksandr.kondratyev@onlyoffice.com>
2025-04-17 11:54:05 +0700

    add Slack components base

---
+++ src/backend/base/langflow/base/slack/__init__.py
@@ -0,0 +1,12 @@
+from .component import Component
+from .inputs import AuthTextInput
+from .outputs import DataOutput, ToolOutput
+from .services import PostMessageOptions
+
+__all__ = [
+    "AuthTextInput",
+    "Component",
+    "DataOutput",
+    "PostMessageOptions",
+    "ToolOutput",
+]
