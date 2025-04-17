15993b5add63a1c7b58e39cffce0c0608d99bb9d
sshakndr <aleksandr.kondratyev@onlyoffice.com>
2025-04-17 14:31:57 +0700

    add Slack conversations.list component

--- src/backend/base/langflow/components/slack/__init__.py
+++ src/backend/base/langflow/components/slack/__init__.py
@@ -1,7 +1,9 @@
 from .slack_chat_post_message import SlackPostMessage
 from .slack_conversations_create_conversation import SlackCreateConversation
+from .slack_conversations_get_list import SlackGetConversations
 
 __all__ = [
     "SlackCreateConversation",
+    "SlackGetConversations",
     "SlackPostMessage"
 ]
